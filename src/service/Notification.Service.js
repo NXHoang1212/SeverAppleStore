0d71cb2d9449cda188e8b23441e7ee5d2f7a9537
xuanhoang <102515365+NXHoang1212@users.noreply.github.com>
2024-08-04 00:55:28 +0700

    push code

--- src/service/Notification.Service.js
+++ src/service/Notification.Service.js
@@ -3,46 +3,23 @@ const { JWT } = require('google-auth-library');
 
 const SCOPES = ['https://www.googleapis.com/auth/firebase.messaging'];
 
-// const getAccessToken = () => {
-//     return new Promise(function (resolve, reject) {
-//         const key = require('../json/service-account.json'); // Đường dẫn tới tệp JSON của bạn
-//         const jwtClient = new JWT(
-//             key.client_email,
-//             null,
-//             key.private_key,
-//             SCOPES,
-//             null
-//         );
-//         jwtClient.authorize(function (err, tokens) {
-//             if (err) {
-//                 reject(err);
-//                 return;
-//             }
-//             resolve(tokens.access_token);
-//         });
-//     });
-// }
-
-class NotificationService {
-    static async getAccessToken() {
-        return new Promise(function (resolve, reject) {
-            const key = require('../../json/service-account.json');
-            const jwtClient = new JWT(
-                key.client_email,
-                null,
-                key.private_key,
-                SCOPES,
-                null
-            );
-            jwtClient.authorize(function (err, tokens) {
-                if (err) {
-                    reject(err);
-                    return;
-                }
-                resolve(tokens.access_token);
-            });
+const getAccessToken = () => {
+    return new Promise(function (resolve, reject) {
+        const key = require('../../json/service-account.json');
+        const jwtClient = new JWT(
+            key.client_email,
+            null,
+            key.private_key,
+            SCOPES,
+            null
+        );
+        jwtClient.authorize(function (err, tokens) {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(tokens.access_token);
         });
-    }
+    });
 }
 
-module.exports = NotificationService;
